a1de4c2d0947fd814403a6cb6e85602eae356a08
quanphung0106-sudo <phungdinhquan16@gmail.com>
2023-01-03 14:35:36 +0700

    create scheduling tags components
    fixed padding and added flex wrap for tags
    
    adjust padding
    
    update

---
+++ components/SchedulingTags/index.tsx
@@ -0,0 +1,204 @@
+import React from 'react'
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemText,
+  Stack,
+  useTheme,
+  Typography,
+  ListItemButton,
+  Tooltip,
+  Chip,
+} from '@mui/material'
+import { ArrowRightIcon } from 'libs/icons'
+import Link from 'next/link'
+
+interface ChipData {
+  label: string
+}
+
+interface ItemProps {
+  primary: string
+  primaryMobile?: string | undefined
+  secondary: string
+  avatar: string
+  tags: ChipData[]
+}
+
+interface TagsProps {
+  items: { title?: string } & { data: ItemProps[] }
+}
+
+const SchedulingTags = ({ items }: TagsProps) => {
+  const theme = useTheme()
+
+  return (
+    <Stack sx={{ backgroundColor: theme.palette.white.main, borderRadius: '16px' }}>
+      <List
+        sx={{
+          '&.MuiList-root': {
+            padding: '15px',
+
+            '& div > .MuiListItemButton-root': {
+              minHeight: '78px',
+              p: '5px 5px',
+              borderRadius: '6px',
+              '&:hover': {
+                cursor: 'pointer',
+                backgroundColor: '#f4f6f8',
+              },
+            },
+          },
+        }}
+      >
+        <Stack gap="10px">
+          {items?.title && (
+            <Link href="/">
+              <Typography
+                variant="h1"
+                fontWeight={600}
+                fontSize="16px"
+                lineHeight="24px"
+                display="flex"
+                alignItems="center"
+                margin="5px 0 5px 0"
+                gap="3px"
+                sx={{ cursor: 'pointer' }}
+              >
+                {items.title}
+                <ArrowRightIcon />
+              </Typography>
+            </Link>
+          )}
+          {items?.data
+            ? items.data.map((item, index) => (
+                <ListItemButton key={index} id="list-item">
+                  <Stack direction="row" alignItems="flex-start" gap="14px">
+                    <Stack
+                      height="68px"
+                      width="44px"
+                      borderRadius="6px"
+                      textAlign="center"
+                      border="1px solid #f5f7f9"
+                      boxShadow="0px 6px 6px 2px rgba(71, 153, 235, 0.04)"
+                    >
+                      <Typography
+                        marginTop="4px"
+                        textTransform="uppercase"
+                        fontSize="14px"
+                        lineHeight="22px"
+                        fontWeight={600}
+                      >
+                        feb
+                      </Typography>
+                      <Typography
+                        fontSize="26px"
+                        lineHeight="38px"
+                        fontWeight={700}
+                        color={theme.palette.blue.main}
+                        marginBottom="4px"
+                      >
+                        7
+                      </Typography>
+                    </Stack>
+                    <Stack minHeight="68px" gap="10px">
+                      <ListItemText
+                        sx={{
+                          '&': {
+                            display: 'flex',
+                            flexDirection: 'column',
+                            m: 0,
+                            '& .MuiTypography-body1': {
+                              fontSize: '14px',
+                              lineHeight: '22px',
+                              fontWeight: 600,
+                              mb: '2px',
+                            },
+                            '& .MuiTypography-body2': {
+                              borderRadius: '2px',
+                              color: '#97989D',
+                              fontSize: '10px',
+                              lineHeight: '16px',
+                              fontWeight: 600,
+                            },
+                          },
+                        }}
+                        disableTypography
+                        primary={
+                          <Tooltip title={item.primary}>
+                            <Typography
+                              textOverflow="ellipsis"
+                              whiteSpace="nowrap"
+                              overflow="hidden"
+                              variant="body1"
+                              maxWidth="250px"
+                            >
+                              {item.primary}
+                            </Typography>
+                          </Tooltip>
+                        }
+                        secondary={
+                          <Stack direction="row" alignItems="center" gap="6px">
+                            <Avatar
+                              sx={{ width: '16px', height: '16px', borderRadius: '8px' }}
+                              alt="Avatar"
+                              src={item?.avatar}
+                            />
+                            <Tooltip title={item.secondary}>
+                              <Typography
+                                variant="body2"
+                                textOverflow="ellipsis"
+                                whiteSpace="nowrap"
+                                overflow="hidden"
+                                maxWidth="230px"
+                              >
+                                {item.secondary}
+                              </Typography>
+                            </Tooltip>
+                          </Stack>
+                        }
+                      />
+                      <List
+                        disablePadding
+                        sx={{
+                          minHeight: '18px',
+                          maxWidth: '250px',
+                          display: 'flex',
+                          gap: '10px',
+                          flexWrap: 'wrap',
+                        }}
+                      >
+                        {item.tags.map((data, index) => (
+                          <ListItem sx={{ width: 'unset' }} disablePadding key={index}>
+                            <Chip
+                              sx={{
+                                backgroundColor: '#f5f5f8',
+                                height: '18px',
+                                p: '2px 8px',
+                                '& span': {
+                                  p: 0,
+                                  fontSize: '9px',
+                                  lineHeight: '14px',
+                                  fontWeight: 600,
+                                  color: '#b6bac6',
+                                  cursor: 'pointer',
+                                },
+                              }}
+                              label={data.label}
+                            />
+                          </ListItem>
+                        ))}
+                      </List>
+                    </Stack>
+                  </Stack>
+                </ListItemButton>
+              ))
+            : ''}
+        </Stack>
+      </List>
+    </Stack>
+  )
+}
+
+export default SchedulingTags
